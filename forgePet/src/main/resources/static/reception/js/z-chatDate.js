adfed727df62b45a1b3f2bbbe397801fd0b0fb5e
MixJade <1114185977@qq.com>
2023-04-14 22:21:58 +0800

    链式调用优化

--- forgePet/src/main/resources/static/reception/js/z-chatDate.js
+++ forgePet/src/main/resources/static/reception/js/z-chatDate.js
@@ -1,11 +1,10 @@
 // 获取工龄天数
 const nowDate = new Date()//工龄毫秒值
-function getDate(createTime) {
+const getDate = createTime => {
     const hours = createTime.split("T")[1]
     const start = Date.parse(createTime)
-    if (start >= nowDate) {
-        return hours
-    }//最快取整
+    if (start >= nowDate) return hours
+    //最快取整
     const dayBad = ~~((nowDate - start) / (86400000))
     if (dayBad > 0) return dayBad + "天前 " + hours
     return hours
