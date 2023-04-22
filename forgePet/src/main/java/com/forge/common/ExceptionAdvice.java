b4dc839ddaeeee160fc25b184aa2b4b2b71c2857
MixJade <1114185977@qq.com>
2023-04-22 11:41:01 +0800

    注释优化

--- forgePet/src/main/java/com/forge/common/ExceptionAdvice.java
+++ forgePet/src/main/java/com/forge/common/ExceptionAdvice.java
@@ -14,28 +14,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionAdvice {
     private static final Logger log = LoggerFactory.getLogger(ExceptionAdvice.class);
 
-    @ExceptionHandler(DuplicateKeyException.class)//关于唯一约束重复插入
+    @ExceptionHandler(DuplicateKeyException.class) // 关于唯一约束重复插入
     public Result doDuplicateKeyException() {
         return Result.error("不能重复插入");
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)//关于空值插入或插入过长
+    @ExceptionHandler(DataIntegrityViolationException.class) // 关于空值插入或插入过长
     public Result doDataIntegrityViolationException(DataIntegrityViolationException e) {
         log.warn("数据完整性违规异常，可能空值插入或插入过长==>" + e);
         return Result.error("数据完整性违约");
     }
 
-    @ExceptionHandler(UnauthorizedException.class)//shiro:没有对应角色
+    @ExceptionHandler(UnauthorizedException.class) // shiro:没有对应角色
     public Result doUnauthorizedException() {
         return Result.error("没有操作权限");
     }
 
-    @ExceptionHandler(UnauthenticatedException.class)//shiro:未登录
+    @ExceptionHandler(UnauthenticatedException.class) // shiro:未登录
     public Result doUnauthenticatedException() {
         return new Result(ResultEnum.NO_LOGIN);
     }
 
-    @ExceptionHandler(Exception.class)// 其他异常，没有预计的异常，发送固定消息
+    @ExceptionHandler(Exception.class) // 其他异常，没有预计的异常，发送固定消息
     public Result doOtherException(Exception ex) {
         log.warn("出现了异常" + ex);
         ex.printStackTrace();
