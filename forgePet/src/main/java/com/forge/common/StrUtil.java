0ee737b0599a5d5283a679f532eda769558e1c01
MixJade <1114185977@qq.com>
2023-04-26 22:47:28 +0800

    字符串工具类

---
+++ forgePet/src/main/java/com/forge/common/StrUtil.java
@@ -0,0 +1,29 @@
+package com.forge.common;
+
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.springframework.lang.Nullable;
+
+/**
+ * 用于判空、加密等字符串操作
+ */
+public final class StrUtil {
+    /**
+     * 判断字段是否为空
+     *
+     * @param str 前端传来的字段
+     * @return 不为空
+     */
+    public static boolean isWhite(@Nullable String str) {
+        return str == null || str.isBlank();
+    }
+
+    /**
+     * 给明文密码加盐加密
+     *
+     * @param pwd 明文密码
+     * @return 加盐加密之后的密码
+     */
+    public static String tranPwd(String pwd) {
+        return (new Md5Hash(pwd, "pet")).toHex();
+    }
+}
