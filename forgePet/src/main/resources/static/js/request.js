9e48312fba9f2bf324dd6cfb76246266e46a971f
MixJade <1114185977@qq.com>
2023-05-23 22:29:11 +0800

    方便内网穿透

--- forgePet/src/main/resources/static/js/request.js
+++ forgePet/src/main/resources/static/js/request.js
@@ -1,6 +1,9 @@
 "use strict";
-axios.defaults.baseURL = "http://localhost:8083/";
-const BACK_URL="http://localhost:8083/";
+// axios.defaults.baseURL = "http://localhost:8083/";
+// const BACK_URL="http://localhost:8083/";
+// 这里用相对路径是让系统可以被内网穿透
+axios.defaults.baseURL = "/";
+const BACK_URL="/";
 axios.interceptors.response.use(resp => {
     if (resp.data["code"] === 401) window.top.location.href = "/login.html"
     return resp;
