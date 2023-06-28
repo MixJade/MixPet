e61a228d98b06bc50cdb746d9312b05d50face43
MixJade <1114185977@qq.com>
2023-06-28 20:57:03 +0800

    静态方法类变为接口

--- shipPet/src/main/java/com/ship/util/UserUtil.java
+++ shipPet/src/main/java/com/ship/util/UserUtil.java
@@ -3,10 +3,10 @@ package com.ship.util;
 import com.ship.security.model.MyUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public final class UserUtil{
-    public static Object getUser() {
-        var people=SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (people instanceof MyUser myUser){
+public interface UserUtil {
+    static Object getUser() {
+        var people = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (people instanceof MyUser myUser) {
             return myUser.role();
         } else return people;
     }
