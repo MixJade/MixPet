7d731b8e6d407b7e65a615e2f1a2f71342f9560e
MixJade <1114185977@qq.com>
2023-08-12 22:21:24 +0800

    解决未登录的跳转路由偶尔失灵问题

--- ship-ui/src/request/reqAPI.ts
+++ ship-ui/src/request/reqAPI.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import {ElMessage} from "element-plus";
-import router from "@/router";
 
 
 //创建axios实例
@@ -19,9 +18,7 @@ reqApi.interceptors.response.use(resp => {
     if (resp.data["code"] != undefined && resp.data["code"] != null) {
         const code = resp.data["code"]
         if (code === 401) {
-            router.replace("/").then((): void => {
-                ElMessage.error("当前未登陆")
-            })
+            location.href = "/"
         } else if (code === 403) {
             ElMessage.warning("未持有操作权限")
         } else if (code === 1) {
