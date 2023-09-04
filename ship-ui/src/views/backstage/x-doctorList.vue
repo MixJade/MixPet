72336d39d70118b8f942c47d280f95cb121e1194
MixJade <1114185977@qq.com>
2023-09-04 14:12:32 +0800

    :bug: 输入框样式修复

--- ship-ui/src/views/backstage/x-doctorList.vue
+++ ship-ui/src/views/backstage/x-doctorList.vue
@@ -91,7 +91,6 @@
             v-model="form.doctorAge"
             format="YYYY-MM-DD"
             placeholder="选择出生日期"
-            style="width: 50%"
             type="date"
             value-format="YYYY-MM-DD"
         />
