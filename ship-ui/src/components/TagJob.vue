f560864ab8b1a5297f4084b47f38115df0ce0aaa
MixJade <1114185977@qq.com>
2023-09-05 20:54:38 +0800

    :art: 科室查看人员优化，README更新

---
+++ ship-ui/src/components/TagJob.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-tag v-if="authLv===8" type="danger">
+    院长
+  </el-tag>
+  <el-tag v-else-if="authLv===6" type="warning">
+    主任
+  </el-tag>
+  <el-tag v-else>
+    {{ getJob(authLv) }}
+  </el-tag>
+</template>
+
+<script lang="ts" setup>
+/*=====医生职业的封装展示标签=====*/
+import {getJob} from "@/utils/JobUtil";
+
+defineProps<{
+  authLv: number
+}>()
+</script>
+
+<style scoped>
+
+</style>
