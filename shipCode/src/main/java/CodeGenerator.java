3b135535aa292dd347ca367a8ce4b0f8d56ba797
MixJade <111669273+MixJade@users.noreply.github.com>
2024-06-29 12:07:39 +0800

    代码生成器配置化

--- shipCode/src/main/java/CodeGenerator.java
+++ shipCode/src/main/java/CodeGenerator.java
@@ -8,23 +8,55 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Collections;
 
-public class CodeGenerator {
+interface MyConfig {
+    /**
+     * 需要生成的表名
+     */
+    String[] tableNames = new String[]{"Adopt"};
+
+    /**
+     * 数据库链接、账号、密码
+     */
+    String url = "jdbc:mysql://localhost:3306/pet_ship?useSSL=true",
+            username = "root",
+            password = "root";
+
     /**
      * 输出目录的路径，到项目名称为止
      */
-    private static final String outputDir = "/home/mixjade/mySoft/MixPet/shipPet";
+    String outputDir = "/home/mixjade/mySoft/MixPet/shipPet";
+
+    /**
+     * 软件包、entity文件的包路径
+     */
+    String parentPack = "com.ship",
+            entityPack = "model.entity";
+
+    /**
+     * MapperXml文件的路径
+     */
+    String mapperXmlDir = "/src/main/resources/com/ship/mapper";
+
+    /**
+     * 文件作者
+     */
+    String author = "MixJade";
+}
+
+
+public class CodeGenerator {
 
     public static void main(String[] args) {
-        FastAutoGenerator.create("jdbc:mysql://localhost:3306/pet_ship?useSSL=true", "root", "root")
+        FastAutoGenerator.create(MyConfig.url, MyConfig.username, MyConfig.password)
                 // ============全局配置============
                 .globalConfig(builder -> {
-                    builder.author("MixJade")
+                    builder.author(MyConfig.author)
                             .disableOpenDir()
-                            .outputDir(outputDir + "/src/main/java"); // 指定输出目录
+                            .outputDir(MyConfig.outputDir + "/src/main/java"); // 指定输出目录
                 })
                 // ===========软件包配置============
-                .packageConfig(builder -> builder.parent("com.ship").entity("model.entity")
-                        .pathInfo(Collections.singletonMap(OutputFile.xml, outputDir + "/src/main/resources/com/ship/mapper")))
+                .packageConfig(builder -> builder.parent(MyConfig.parentPack).entity(MyConfig.entityPack)
+                        .pathInfo(Collections.singletonMap(OutputFile.xml, MyConfig.outputDir + MyConfig.mapperXmlDir)))
                 // 使用Freemarker引擎模板
                 .templateEngine(new FreemarkerTemplateEngine())
                 // ============模板配置============
@@ -37,11 +69,17 @@ public class CodeGenerator {
                         .xml("/templates/mapper.xml")
                         .controller("/templates/controller.java"))
                 // ============策略配置============
-                .strategyConfig(builder -> builder.entityBuilder().enableFileOverride()
+                .strategyConfig(builder -> builder
+                        // 需要生成的表名,注掉则为生成所有表
+                        .addInclude(MyConfig.tableNames)
+                        // 覆盖已经生成的entity文件
+                        .entityBuilder().enableFileOverride()
+                        // entity的字段自动填充策略,最好关掉
                         .addTableFills(
                                 new Column("create_time", FieldFill.INSERT),
                                 new Column("update_time", FieldFill.INSERT_UPDATE)
                         )
+                        // mapper接口打上Mapper注解
                         .mapperBuilder().mapperAnnotation(Mapper.class)
                         .serviceBuilder()
                         .controllerBuilder().enableRestStyle())
